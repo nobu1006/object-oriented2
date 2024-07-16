04cf69ada4495e52cbf37b00ccadfafc6538eb19
Nobuhiko Tobita <nobu1006@gmail.com>
2024-07-16 12:11:13 +0900

    create Clazz

--- src/human/Clazz.java
+++ src/human/Clazz.java
@@ -16,6 +16,26 @@ public class Clazz {
         return num;
     }
 
+    public double getMensAgeAverage() {
+        int total = 0;
+        for (Human h : members) {
+            if ("男性".equals(h.getGender())) {
+                total += h.getAge();
+            }
+        }
+        return  total / (double)getMensNum();
+    }
+
+    public double getWomensAgeAverage() {
+        int total = 0;
+        for (Human h : members) {
+            if ("女性".equals(h.getGender())) {
+                total += h.getAge();
+            }
+        }
+        return  total / (double)getWomensNum();
+    }
+
     public int getWomensNum() {
         int num = 0;
         for (Human human : members) {
